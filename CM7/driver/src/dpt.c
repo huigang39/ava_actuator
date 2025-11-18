51d3420cbfa86803eec1a61510baf84d5dd4fada
huigang39 <huigang39@outlook.com>
2025-11-18 16:33:03 +0800

    feat

--- CM7/driver/src/dpt.c
+++ CM7/driver/src/dpt.c
@@ -5,8 +5,8 @@
 #include "buffer_cfg.h"
 #include "dpt.h"
 
-volatile dpt_raw_t dpt_raw;
-volatile f32       dpt_inner_theta;
+volatile dpt_raw_t g_dpt_raw;
+volatile f32       g_dpt_inner_theta;
 
 void
 dpt_init(void)
@@ -19,13 +19,13 @@ dpt_raw_t
 dpt_get_raw(void)
 {
         HAL_UART_Transmit_DMA(g_sensor_uart, &DPT_TX_BUF, sizeof(DPT_TX_BUF));
-        dpt_raw.inner = (u32)LF(24) - ((u32)DPT_RX_BUF[5] << 16 | DPT_RX_BUF[4] << 8 | DPT_RX_BUF[3]);
-        return dpt_raw;
+        g_dpt_raw.inner = (u32)LF(24) - ((u32)DPT_RX_BUF[5] << 16 | DPT_RX_BUF[4] << 8 | DPT_RX_BUF[3]);
+        return g_dpt_raw;
 }
 
 f32
 dpt_get_inner_theta(void)
 {
-        dpt_inner_theta = ((f32)dpt_get_raw().inner / (f32)LF(24)) * TAU;
-        return dpt_inner_theta;
+        g_dpt_inner_theta = ((f32)dpt_get_raw().inner / (f32)LF(24)) * TAU;
+        return g_dpt_inner_theta;
 }
