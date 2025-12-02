1b506a4e7b204d3ff97dd15ee6e547e00d378b19
huigang39 <huigang39@outlook.com>
2025-12-02 20:17:51 +0800

    feat

--- CM7/user/src/comm_shm.c
+++ CM7/user/src/comm_shm.c
@@ -60,7 +60,7 @@ comm_shm_sync_rt(comm_shm_t *comm_shm, foc_t *foc)
         comm_shm_sync_pid_param(comm_shm, foc);
         foc_set_ref(foc, comm_shm->rt.ref_pvct);
 
-        comm_shm->rt.fdb_pvct = lo->fdb_pvct;
+        comm_shm->rt.fdb_pvct = foc_get_fdb(foc);
 }
 
 static void
