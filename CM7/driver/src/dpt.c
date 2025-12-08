fb466587515dac177dc64b6a8a340294667328dd
huigang39 <huigang39@outlook.com>
2025-12-08 21:37:55 +0800

    feat

--- CM7/driver/src/dpt.c
+++ CM7/driver/src/dpt.c
@@ -19,8 +19,8 @@ dpt_raw_t
 dpt_get_raw(void)
 {
         HAL_UART_Transmit_DMA(g_sensor_uart, &g_dpt_tx_buf, sizeof(g_dpt_tx_buf));
-        g_dpt_raw.inner  = bswap32(g_dpt_rx_buf[0]);
-        g_dpt_raw.outer  = bswap32(g_dpt_rx_buf[3]);
+        g_dpt_raw.inner  = (u32)BIT(DPT_BITS) - ((u32)g_dpt_rx_buf[2] << 16 | g_dpt_rx_buf[1] << 8 | g_dpt_rx_buf[0]);
+        g_dpt_raw.outer  = (u32)BIT(DPT_BITS) - ((u32)g_dpt_rx_buf[5] << 16 | g_dpt_rx_buf[4] << 8 | g_dpt_rx_buf[3]);
         g_dpt_raw.status = g_dpt_rx_buf[6];
         g_dpt_raw.crc    = g_dpt_rx_buf[7];
         return g_dpt_raw;
@@ -29,13 +29,14 @@ dpt_get_raw(void)
 f32
 dpt_get_inner_theta(void)
 {
-        g_dpt_inner_theta = ((f32)dpt_get_raw().inner / (f32)BIT(DPT_BITS)) * TAU;
-        return g_dpt_inner_theta * 10.0f;
+        g_dpt_inner_theta = ((f32)g_dpt_raw.inner / (f32)BIT(DPT_BITS)) * TAU;
+        return g_dpt_inner_theta;
 }
 
 f32
 dpt_get_outer_theta(void)
 {
-        g_dpt_outer_theta = ((f32)dpt_get_raw().outer / (f32)BIT(DPT_BITS)) * TAU;
-        return g_dpt_outer_theta;
+        dpt_get_raw();
+        g_dpt_outer_theta = ((f32)g_dpt_raw.outer / (f32)BIT(DPT_BITS)) * TAU;
+        return g_dpt_outer_theta * 10.0f;
 }
